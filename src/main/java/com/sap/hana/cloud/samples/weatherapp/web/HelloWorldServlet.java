cd06e48c5d6420c1e32f4b98603e2582aef49b0b
Matthias Steiner <matthias.steiner@inscope.net>
2015-05-23 03:08:41 +0200

    Step 3 - Adding authentication
    Change-Id: I6ccd0da759c9ddd086f4c62695367d07ef76a3c8
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

--- src/main/java/com/sap/hana/cloud/samples/weatherapp/web/HelloWorldServlet.java
+++ src/main/java/com/sap/hana/cloud/samples/weatherapp/web/HelloWorldServlet.java
@@ -1,10 +1,15 @@
 package com.sap.hana.cloud.samples.weatherapp.web;
 
 import java.io.IOException;
+
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sap.security.auth.login.LoginContextFactory;
 
 /**
  * Servlet implementation class HelloWorldServlet
@@ -23,15 +28,35 @@ public class HelloWorldServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello World!");
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        String user = request.getRemoteUser();
+        
+        if (user != null)
+        {
+            response.getWriter().println("Hello, " + user);
+        }
+        else
+        {
+            LoginContext loginContext;
+            
+    	   try 
+            {
+                loginContext =  LoginContextFactory.createLoginContext("FORM");
+    		    loginContext.login();
+                response.getWriter().println("Hello, " + request.getRemoteUser());
+            } 
+            catch (LoginException ex) 
+            {
+                 ex.printStackTrace();
+    	   }
+        }
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+        doGet(request, response);
+    }
 }
