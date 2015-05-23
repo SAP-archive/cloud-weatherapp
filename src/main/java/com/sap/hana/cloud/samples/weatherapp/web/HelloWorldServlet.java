4f51ebba7f659fb0537240ab76823d53f0c1c78a
Matthias Steiner <matthias.steiner@inscope.net>
2015-05-23 02:59:55 +0200

    Step 1 - Hello World
    Change-Id: Id26d9676436a1f4d64f7a2e7d58b5927168c917b
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

---
+++ src/main/java/com/sap/hana/cloud/samples/weatherapp/web/HelloWorldServlet.java
@@ -0,0 +1,37 @@
+package com.sap.hana.cloud.samples.weatherapp.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
