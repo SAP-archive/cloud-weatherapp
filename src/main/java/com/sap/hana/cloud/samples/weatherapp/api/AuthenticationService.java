4db60ed5f1e9c69a70c6a49d5b1f4fd47d697e95
Matthias Steiner <matthias.steiner@inscope.net>
2015-05-23 03:25:53 +0200

    Step 5 - RESTful Service Provisioning
    Change-Id: Iea9a31adf5a2da0885a372cf238ea68a1cac0ad9
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

---
+++ src/main/java/com/sap/hana/cloud/samples/weatherapp/api/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.sap.hana.cloud.samples.weatherapp.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ * {@link AuthenticationService}
+ * 
+ * @author Matthias Steiner
+ * @version 0.1
+ */
+@Path("/auth")
+@Produces({ MediaType.APPLICATION_JSON })
+public class AuthenticationService 
+{
+	@GET
+	@Path("/")
+	@Produces({ MediaType.TEXT_PLAIN })
+	public String getRemoteUser(@Context SecurityContext ctx)
+	{
+		String retVal = "anonymous"; 
+		
+		try
+		{
+			retVal = ctx.getUserPrincipal().getName();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace(); // lazy 
+		}
+		
+		
+		return retVal;
+	}
+}
